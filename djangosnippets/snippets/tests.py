5fe1fa654141ee7cc83bb3fa700530734f8bb936
marusho <mrsho34@gmail.com>
2021-07-24 23:31:57 +0900

    add: detailページの作成

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -45,16 +45,37 @@ class TopPageRenderSnippetsTest(TestCase):
         self.assertContains(response, self.user.username)
 
 
+class SnippetDetailTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email="test@example.com",
+            password="secret",
+        )
+        self.snippet = Snippet.objects.create(
+            title="タイトル",
+            code="コード",
+            description="解説",
+            created_by=self.user,
+        )
+
+    def test_should_resolve_snippet_detail(self):
+        found = resolve("/snippets/1/")
+        self.assertEqual(snippet_detail, found.func)
+
+    def test_should_use_expected_template(self):
+        response = self.client.get("/snippets/%s/" % self.snippet.id)
+        self.assertTemplateUsed(response, "snippets/snippet_detail.html")
+
+    def test_top_page_returns_200_and_expected_heading(self):
+        response = self.client.get("/snippets/%s/" % self.snippet.id)
+        self.assertContains(response, self.snippet.title, status_code=200)
+
 class CreateSnippetTest(TestCase):
     def test_should_resolve_snippet_new(self):
         found = resolve("/snippets/new/")
         self.assertEqual(snippet_new, found.func)
 
-
-class SnippetDetailTest(TestCase):
-    def test_should_resolve_snippet_detail(self):
-        found = resolve("/snippets/1/")
-        self.assertEqual(snippet_detail, found.func)
 
 
 class EditSnippetTest(TestCase):
--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from snippets.models import Snippet
-
-# Create your views here.
 
 def top(request):
     snippets = Snippet.objects.all()
@@ -16,5 +14,6 @@ def snippet_edit(request):
     return HttpResponse('スニペットの編集')
 
 def snippet_detail(request, snippet_id):
-    return HttpResponse('スニペットの詳細閲覧')
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    return render(request, "snippets/snippet_detail.html", {"snippet": snippet})
 
