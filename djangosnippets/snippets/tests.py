e67b5d650927fb96b9cc5b76ce640bdd1e541663
marusho <mrsho34@gmail.com>
2021-07-24 16:53:54 +0900

    feat: Hello world

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -2,8 +2,6 @@ from django.test import TestCase
 from django.http import HttpRequest
 from snippets.views import top
 
-# Create your tests here.
-
 
 class TopPageViewTest(TestCase):
     def test_top_returns_200(self):
@@ -15,4 +13,13 @@ class TopPageViewTest(TestCase):
     def test_top_returns_expected_content(self):
         request = HttpRequest()
         response = top(request)
+        self.assertEqual(b"Hello, world!", response.content)
+
+class TopPageTest(TestCase):
+    def test_top_returns_200(self):
+        response = self.client.get("/")
+        self.assertEqual(200, response.status_code)
+
+    def test_top_returns_expected_content(self):
+        response = self.client.get("/")
         self.assertEqual(b"Hello, world!", response.content)
