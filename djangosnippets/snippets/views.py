73ecad393dcdb51cbb831d99e54db5f0f47ae33c
marusho <mrsho34@gmail.com>
2021-07-24 17:11:37 +0900

    add: routingの追加

--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def top(request):
     return HttpResponse(b"Hello, world!")
+
+def snippet_new(request):
+    return HttpResponse('スニペットの登録')
+
+def snippet_edit(request):
+    return HttpResponse('スニペットの編集')
+
+def snippet_detail(request, snippet_id):
+    return HttpResponse('スニペットの詳細閲覧')
+
